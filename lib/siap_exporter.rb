e1f75298efae19121dd4413037b86c0d8a1f440f
Lionel Hsu <hsulionel@gmail.com>
2016-09-29 11:04:37 -0300

    Ventas mediante fixed width dsl

--- lib/siap_exporter.rb
+++ lib/siap_exporter.rb
@@ -1,18 +1,43 @@
 require 'fixed_width_dsl'
 
 module SiapExporter
-  TIPO_FACTURA = {'A'=>1}
+  TIPO_COMPROBANTE = {'A'=>1}
   TIPO_ALICUOTA = {'21%'=>5}
 
   class ComprasVentas
 
     AlicuotaVenta = FixedWidthDSL.define do
-      field :tipo_comprobante, 3, :integer, '0'
-      field :punto_de_venta,   5, :integer, '0'
-      field :factura,         20, :integer, '0'
-      field :gravado,         15, :integer, '0'
-      field :tipo_alicuota,    4, :integer, '0'
-      field :impuesto,        15, :integer, '0'
+      field :tipo_comprobante,    3,   :integer,  '0'
+      field :punto_de_venta,      5,   :integer,  '0'
+      field :numero_comprobante,  20,  :integer,  '0'
+      field :gravado,             15,  :integer,  '0'
+      field :tipo_alicuota,       4,   :integer,  '0'
+      field :impuesto,            15,  :integer,  '0'
+    end
+
+    Venta = FixedWidthDSL.define do
+      field :fecha,                            8,   :integer,  '0'
+      field :tipo_comprobante,                 3,   :integer,  '0'
+      field :punto_de_venta,                   5,   :integer,  '0'
+      field :numero_comprobante,               20,  :integer,  '0'
+      field :numero_comprobante_hasta,         20,  :integer,  '0'
+      field :codigo_documento_comprador,       2,   :integer,  '0'
+      field :numero_identificacion_comprador,  20,  :integer,  '0'
+      field :denominacion_comprador,           30,  :string,   '-'
+      field :total,                            15,  :integer,  '0'
+      field :no_gravado,                       15,  :integer,  '0'
+      field :no_categorizados,                 15,  :integer,  '0'
+      field :exento,                           15,  :integer,  '0'
+      field :impuestos_nacionales,             15,  :integer,  '0'
+      field :ingresos_brutos,                  15,  :integer,  '0'
+      field :impuestos_municipales,            15,  :integer,  '0'
+      field :impuestos_internos,               15,  :integer,  '0'
+      field :moneda,                           3,   :string,   '0'
+      field :cambio,                           10,  :integer,  '0'
+      field :cantidad_de_alicuotas,            1,   :integer,  '0'
+      field :codigo_de_operacion,              1,   :integer,  '0'
+      field :otros_tributos,                   15,  :integer,  '0'
+      field :vencimiento_de_pago,              8,   :integer,  '0'
     end
 
     def self.generate comprobantes
@@ -39,38 +64,33 @@ module SiapExporter
     private
 
     def alicuotas_venta comprobante
-      comprobante = comprobante.dup
-      comprobante[:tipo_comprobante] = TIPO_FACTURA[comprobante[:tipo_factura]]
-      comprobante[:gravado] = comprobante[:gravado_21]
-      comprobante[:tipo_alicuota] = TIPO_ALICUOTA['21%']
-      comprobante[:impuesto] = comprobante[:iva_21]
+      comprobante = comprobante.merge(
+        tipo_comprobante: TIPO_COMPROBANTE[comprobante[:tipo_comprobante]],
+        gravado: comprobante[:gravado_21],
+        tipo_alicuota: TIPO_ALICUOTA['21%'],
+        impuesto: comprobante[:iva_21])
       AlicuotaVenta.apply comprobante
     end
 
     def venta comprobante
-      fecha = comprobante[:fecha].gsub('-', '')
-      cantidad_de_alicuotas = 1
-      format '%8d%03d%05d%020d%020d%02d%020d%-30s' \
-        '%015d%015d%015d%015d%015d%015d%015d%015d' \
-        'PES0001000000%1d0%015d00000000',
-        fecha,
-        TIPO_FACTURA.fetch(comprobante[:tipo_factura]),
-        comprobante[:punto_de_venta],
-        comprobante[:factura],
-        comprobante[:factura],
-        80,
-        comprobante[:cuit],
-        comprobante[:cliente],
-        comprobante[:total],
-        comprobante[:no_gravado],
-        comprobante.fetch(:no_categorizado, 0),
-        comprobante.fetch(:exento, 0),
-        comprobante.fetch(:impuestos_nacionales, 0),
-        comprobante.fetch(:ingresos_brutos, 0),
-        comprobante.fetch(:impuestos_municipales, 0),
-        comprobante.fetch(:impuestos_internos, 0),
-        cantidad_de_alicuotas,
-        comprobante.fetch(:otros_tributos, 0)
+      comprobante = comprobante.merge(
+        fecha: comprobante[:fecha].gsub('-', ''),
+        tipo_comprobante: TIPO_COMPROBANTE[comprobante[:tipo_comprobante]],
+        numero_comprobante_hasta: comprobante[:numero_comprobante],
+        codigo_documento_comprador: 80,
+        total: comprobante[:total],
+        no_categorizados: 0,
+        impuestos_nacionales: 0,
+        ingresos_brutos: 0,
+        impuestos_municipales: 0,
+        impuestos_internos: 0,
+        moneda: 'PES',
+        cambio: 1000000,
+        cantidad_de_alicuotas: 1,
+        codigo_de_operacion: 0,
+        otros_tributos: 0,
+        vencimiento_de_pago: 0)
+      Venta.apply comprobante
     end
   end
 end
