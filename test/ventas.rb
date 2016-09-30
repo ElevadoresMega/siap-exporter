3b2d1132134f0ee2a8112029b2c6835eda058607
Eloy Espinaco <eloyesp@gmail.com>
2016-09-30 11:41:49 -0300

    Factura B

---
+++ test/ventas.rb
@@ -0,0 +1,20 @@
+require_relative '../lib/siap_exporter'
+
+comprobante = {
+  fecha: '2016-07-04',
+  denominacion_comprador: 'Tito Puente',
+  numero_identificacion_comprador: 27224686604,
+  tipo_comprobante: 'A',
+  punto_de_venta: 4,
+  numero_comprobante: 1220,
+  gravado_21: 144950,
+  iva_21: 30440,
+  no_gravado: 1873400,
+  exento: 1765741,
+  total: 3814531
+}
+
+factura_b = comprobante.merge(tipo_comprobante: 'B')
+
+venta = SiapExporter::ComprasVentas.generate([factura_b])[:ventas]
+assert venta.match('006'), 'Factura B funciona'
